3530aa91f8bf68fbd7736548eabeb9e2cba805d6
Aleksei Cherniavskii <aleksei_cherniavskii@epam.com>
2019-11-07 15:49:56 +0300

    init migration scripts; init config.yml

---
+++ migrations/1.js
@@ -0,0 +1,7 @@
+module.exports = function runMigration(migration) {
+    /*
+    The empty migration to represent the initial import that we did manually by command:
+        contentful space import --content-file .\import\export.json
+    */
+    return;
+};
